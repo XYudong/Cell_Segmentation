fixed feature normalization bug

Unet_training.py
@@ -9,7 +9,7 @@ from tensorflow.python.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.python.keras import backend as K
 
 tf.keras.backend.set_image_data_format('channels_last')
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # crop this smaller square portions from the original image(s), which are resized to this value (68 pixels by 68 pixels)
 S_SIZE = 68
@@ -132,6 +132,7 @@ if __name__ == '__main__':
     dataset = 'HumanN4_MouseN1'
     im_path = 'DataSet_label/Human_Muscle_PF573228/train/img'
     mask_path = 'DataSet_label/Human_Muscle_PF573228/train/mask'
-    model_resume = 'results/model/Human_Muscle/vUnet_DMSO_N4_FAK_N4_04.hdf5'
-    for lr, num in zip([5e-4], ['05']):
+    model_resume = 'results/model/Human_Muscle/vUnet_HumanN4_MouseN1_05.hdf5'
+
+    for lr, num in zip([5e-4], ['temp']):
         train_vUnet(dataset, lr=lr, epochs=30, trial_num=num, model_path=model_resume)

analysis_utils.py
@@ -17,7 +17,7 @@ def draw_contours(mask):
 
 class FeatureExtractor:
     GRAY_THRESHOLD = 30
-    AREA_THRESHOLD = 2500       # in order to ignore outliers
+    AREA_THRESHOLD = 2500           # in order to ignore outliers
 
     def __init__(self):
         self.mask = None
@@ -89,7 +89,7 @@ class FeatureExtractor:
         hull_area = cv2.contourArea(hull)
         self.solidity = float(self.max_area) / hull_area
 
-    def get_feature_vec(self, mask):
+    def get_fea_vec_0(self, mask):
         # combine all the feature values together
         self.preprocess_mask(mask)
 
@@ -105,11 +105,23 @@ class FeatureExtractor:
                             self.centroids_dist], dtype='float32')
         return fea_vec
 
+    def get_fea_vec_1(self, mask):
+        # just two features
+        self.preprocess_mask(mask)
+
+        self.cal_contour_and_area()
+        self.cal_solidity()
+
+        fea_vec = np.array([self.ex_area, self.num_colony])
+
+        return fea_vec
+
 
 if __name__ == '__main__':
+    # test a single image
     mask_path = 'results/predict/MM_DMSO_N3/H4_M1/predMask/predMask_40.png'
     mask = cv2.imread(mask_path, 0)
 
     extractor = FeatureExtractor()
-    vec = extractor.get_feature_vec(mask)
+    vec = extractor.get_fea_vec_0(mask)
     print(vec)

cell_analysis.py
@@ -8,54 +8,75 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
 
-def save_fea_vector(path_list):
-    for mask_path in path_list:
-        img_list = sorted(glob.glob(path.join(mask_path, '*.png')))
-        print(str(len(img_list)) + ' images')
+def save_fea_vector(path_list, out_path):
+    vectors = []
+    idx_FAK = [0]
+    count = 0
+    for single_trial_path in path_list:
+        # for each trial folder
+        imgs_list = sorted(glob.glob(path.join(single_trial_path, '*.png')))
+        print(str(len(imgs_list)) + ' images in ' + single_trial_path)
         extractor = FeatureExtractor()
-        vectors = []
-        for img_path in img_list:
-            mask = cv2.imread(img_path, 0)
-            fea_vec = extractor.get_feature_vec(mask)
+
+        for single_img_path in imgs_list:
+            # for each mask
+            mask = cv2.imread(single_img_path, 0)
+            fea_vec = extractor.get_fea_vec_0(mask)         # ndarray
             vectors.append(fea_vec)
 
-        vectors = np.stack(vectors, axis=0)
-        # column-wise normalization
-        for j in range(vectors.shape[1]):
-            v = vectors[:, j]
-            vectors[:, j] = (v - np.mean(v)) / (np.std(v) + 1e-6)
+        if count < 3:       # number of FAK_trial folders
+            idx_FAK.append(idx_FAK[-1] + len(imgs_list))
+            count += 1
 
-        print('shape: ', vectors.shape)  # (N, F)
-        np.save(mask_path + '/fea_vectors_00.npy', vectors)
-        print('features saved: ', mask_path)
+    vectors = np.stack(vectors, axis=0)
+    # column-wise normalization on the whole data matrix
+    for j in range(vectors.shape[1]):
+        v = vectors[:, j]
+        vectors[:, j] = (v - np.mean(v)) / (np.std(v) + 1e-6)
+
+    # save FAK trial data separately
+    FAK_names = ['FAK_' + s for s in ['N4', 'N5', 'N6']]
+    for i, dataset in enumerate(FAK_names):
+        filename = dataset + '_fea_vectors_00.npy'
+        np.save(path.join(out_path, filename), vectors[idx_FAK[i]:idx_FAK[i+1], :])
+
+    np.save(path.join(out_path, 'DMSO_fea_vectors_00.npy'), vectors[idx_FAK[-1]:, :])
+    print('shape: ', vectors.shape)  # (Num, Fea)
+    print('features saved')
+
+
+def load_fea(path_list):
+    # loading feature vectors
+    fea_vectors = []
+    idx_list = [0]
+    for fea_path in path_list:
+        v = np.load(fea_path)
+        fea_vectors.append(v)
+        idx_list.append(v.shape[0] + idx_list[-1])
+        print('feature vectors shape: ', v.shape)
+
+    fea_vectors = np.concatenate(fea_vectors, axis=0)
+    return fea_vectors, idx_list
 
 
 def visualize_pca(path_list):
-    # loading feature vectors
-    fea_vectors = []
-    idx = [0]
-    for fea_path in path_list:
-        v = np.load(fea_path + '/fea_vectors_00.npy')
-        fea_vectors.append(v)
-        idx.append(v.shape[0] + idx[-1])
+    fea_vectors, idx = load_fea(path_list)
 
-    fea_vectors = np.concatenate(fea_vectors, axis=0)
-    print(fea_vectors.shape)
-
-    FAK_names = ['FAK_' + s for s in ['N1', 'N2', 'N3', 'N4']]
-    DMSO_names = ['DMSO_' + s for s in ['N1', 'N2', 'N3', 'N4']]
+    FAK_names = ['FAK_' + s for s in ['N4', 'N5', 'N6']]
+    DMSO_names = ['DMSO_' + s for s in ['N4, N6']]
 
     # initialize PCA
     pca = PCA(n_components=2)
     fea_low = pca.fit_transform(fea_vectors)
+    # fea_low = fea_vectors
 
     plt.figure()
     for i, dataset in enumerate(FAK_names):
-        plt.scatter(fea_low[idx[i]:idx[i + 1] - 1, 0], fea_low[idx[i]:idx[i + 1] - 1, 1], marker='o', label=dataset)
+        plt.scatter(fea_low[idx[i]:idx[i + 1], 0], fea_low[idx[i]:idx[i + 1], 1], marker='o', label=dataset)
 
     for i, dataset in enumerate(DMSO_names):
         i += len(FAK_names)
-        plt.scatter(fea_low[idx[i]:idx[i + 1] - 1, 0], fea_low[idx[i]:idx[i + 1] - 1, 1], marker='+', label=dataset)
+        plt.scatter(fea_low[idx[i]:idx[i + 1], 0], fea_low[idx[i]:idx[i + 1], 1], marker='+', label=dataset)
 
     plt.title('PCA of Cell Features')
     plt.legend()
@@ -64,31 +85,23 @@ def visualize_pca(path_list):
 
 
 def visualize_tsne(path_list):
-    fea_vectors = []
-    idx = [0]
-    for fea_path in path_list:
-        v = np.load(fea_path + '/fea_vectors_00.npy')
-        fea_vectors.append(v)
-        idx.append(v.shape[0] + idx[-1])
+    fea_vectors, idx = load_fea(path_list)
 
-    fea_vectors = np.concatenate(fea_vectors, axis=0)
-    print(fea_vectors.shape)
+    FAK_names = ['FAK_' + s for s in ['N4', 'N5', 'N6']]
+    DMSO_names = ['DMSO_' + s for s in ['N4, N6']]
 
-    FAK_names = ['FAK_' + s for s in ['N1', 'N2', 'N3', 'N4']]
-    DMSO_names = ['DMSO_' + s for s in ['N1', 'N2', 'N3', 'N4']]
-
-    for perplexity in [20, 25, 30, 35, 40]:
+    for perplexity in [20, 25, 30, 35, 15]:
         tsne = TSNE(n_components=2, perplexity=perplexity,
                     learning_rate=20, n_iter=5000)
         fea_low = tsne.fit_transform(fea_vectors)
 
         plt.figure()
         for i, dataset in enumerate(FAK_names):
-            plt.scatter(fea_low[idx[i]:idx[i+1] - 1, 0], fea_low[idx[i]:idx[i+1] - 1, 1], marker='o', label=dataset)
+            plt.scatter(fea_low[idx[i]:idx[i+1], 0], fea_low[idx[i]:idx[i+1], 1], marker='o', label=dataset)
 
         for i, dataset in enumerate(DMSO_names):
             i += len(FAK_names)
-            plt.scatter(fea_low[idx[i]:idx[i+1] - 1, 0], fea_low[idx[i]:idx[i+1] - 1, 1], marker='+', label=dataset)
+            plt.scatter(fea_low[idx[i]:idx[i+1], 0], fea_low[idx[i]:idx[i+1], 1], marker='+', label=dataset)
 
         plt.title('t-SNE of Cell Features, perplexity: ' + str(perplexity))
         plt.legend()
@@ -97,18 +110,26 @@ def visualize_tsne(path_list):
 
 
 if __name__ == '__main__':
-    pred_mask_list = ['results/predict/MM_FAK_N1/H4_M1/predMask',
-                      'results/predict/MM_FAK_N2/H4_M1/predMask',
-                      'results/predict/MM_FAK_N3/H4_M1/predMask',
-                      'results/predict/MM_FAK_N4/H4_M1/predMask',
-                      'results/predict/MM_DMSO_N1/H4_M1/predMask',
-                      'results/predict/MM_DMSO_N2/H4_M1/predMask',
-                      'results/predict/MM_DMSO_N3/H4_M1/predMask',
-                      'results/predict/MM_DMSO_N4/H4_M1/predMask']
+    root = 'results/predict/HumanMuscle'
+    # predicted masks
+    mask_list = ['HM_FAK_N4/N4_model_03/predMask',
+                 'HM_FAK_N5/N4_model_03/predMask',
+                 'HM_FAK_N6/N4_model_03/predMask',
+                 'HM_DMSO_N4/N4_model_04/predMask',
+                 'HM_DMSO_N6/N4_model_04/predMask']
+    pred_mask_list = [path.join(root, mask) for mask in mask_list]
+    fea_out_path = root
+
+    # feature matrices extracted from the masks
+    fea_list = ['FAK_N4_fea_vectors_00.npy',
+                'FAK_N5_fea_vectors_00.npy',
+                'FAK_N6_fea_vectors_00.npy',
+                'DMSO_fea_vectors_00.npy']
+    fea_list = [path.join(fea_out_path, fea) for fea in fea_list]
 
     # First step:
-    # save_fea_vector(pred_mask_list)
+    # save_fea_vector(pred_mask_list, fea_out_path)
 
     # Second step:
-    # visualize_pca(pred_mask_list)
-    visualize_tsne(pred_mask_list)
+    # visualize_pca(fea_list)
+    visualize_tsne(fea_list)

test.py
@@ -3,8 +3,6 @@ from os import path
 import glob
 import cv2
 import matplotlib.pyplot as plt
-import heapq
-import tensorflow as tf
 
 # read_msk('DataSet_label/FAK_N1/img/C1_01_1_1_Bright Field_001.tif')
 
@@ -41,34 +39,15 @@ import tensorflow as tf
 #     return out
 
 
-def test_fn():
-    # img0 = cv2.imread('results/predict/HM_FAK_N6/N4_model_03/predMask/predMask_0029.png', 0)
-
-    img = np.zeros((100, 100)).astype(np.uint8)
-    cv2.circle(img, (50, 50), 20, 1, thickness=-1)
-    cv2.circle(img, (50, 50), 10, 0, thickness=-1)
-
-    cnts, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    areas = [cv2.contourArea(c) for c in cnts]
-    print(areas)
-
-    print(np.pi * 10**2)
-    print(np.pi * 20**2)
-
-    plt.figure()
-    plt.imshow(img, 'gray')
-
-    plt.show()
+def fn():
+    a = 'aaa'
+    b = ['bbb', 'ccc', 'ddd']
+    print(b[0:2])
 
 
 if __name__ == '__main__':
     # extract_fea()
-    # test_fn()
-    a = [-2, 2]
-    print(np.linalg.norm(a))
-
-
-
+    fn()
 
 
 